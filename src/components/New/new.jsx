d94747cf2b29cab6fe2758a5ddd45b733759ec19
Sergio1-Git <mercadosergio754@gmail.com>
2023-12-15 15:30:21 -0500

    Last changes

--- src/components/New/new.jsx
+++ src/components/New/new.jsx
@@ -5,10 +5,11 @@ function New() {
   return (
     <div className='new'>
       <h1 className='new__title'>New</h1>
-      {recentNews.map((post) => (
+      {recentNews.map((post, index) => (
         <div key={post.id} className='new__post'>
           <span className='new__post__title'>{post.title}</span>
           <p className='new__post__text'>{post.description}</p>
+          {index < recentNews.length - 1 && <div className='separator' />}
         </div>
       ))}
     </div>
